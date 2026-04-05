7bbe2d0074b2315941526295fcb4d18d8d113a46
Minh Nguyen <dangminh214@gmail.com>
2026-04-05 17:34:48 +0200

    adjust auto save config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -6,6 +6,22 @@ local options = {
     cpp = { "clang_format" },
   },
 
+   -- configure indent directly here
+  formatters = {
+    clang_format = {
+      args = {
+        "--style={BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: Never}"
+      },
+    },
+    stylua = {
+      args = {
+        "--indent-type", "Spaces",
+        "--indent-width", "4",
+        "-"
+      },
+    },
+  },
+
   format_on_save = {
   -- These options will be passed to conform.format()
      timeout_ms = 500,
