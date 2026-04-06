4a81f33b544591610b2267e053be024cabe19cf5
Minh Nguyen <dangminh214@gmail.com>
2026-04-06 10:15:12 +0200

    add codelldb plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,7 +46,30 @@ return {
   {
       "kdheepak/lazygit.nvim",
       lazy = false,
-  }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    lazy = false,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    lazy = false,
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+
+      -- auto open/close UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+      end
+    end,
+  },
+
 
 
   -- test new blink
