b9b511543a28792ada6b3da7346a76a2e263eb94
minhdangnguyenn <dangminh214@gmail.com>
2026-05-22 23:26:04 +0200

    adjust comments

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,11 @@
--- Use NvChad's default LSP settings (keymaps, capabilities, etc.)
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- Load NvChad's default LSP settings (keymaps, capabilities, etc.)
+local nvchad_lsp = require("nvchad.configs.lspconfig")
+local on_attach = nvchad_lsp.on_attach
+local capabilities = nvchad_lsp.capabilities
 
 local lspconfig = require "lspconfig"
 
--- List all the servers you want here
+-- List all LSP servers you want to use
 local servers = { "clangd", "cmake", "rust_analyzer" }
 
 for _, server in ipairs(servers) do
@@ -14,13 +15,18 @@ for _, server in ipairs(servers) do
   }
 end
 
--- config diagnostics
-vim.diagnostic.config({
-  virtual_text = {
-    prefix = ">>",   -- symbol stays before error
-    spacing = 2,    -- add a space between code and error
-  },
-  signs = true,      -- keep the signs
-  underline = true,  -- unterline error
-  update_in_insert = false,
-})
+-- Override diagnostic display once everything is loaded
+vim.schedule(function()
+  vim.diagnostic.config({
+    virtual_text = {
+      prefix = ">>",  -- text shown before the error message
+      spacing = 2,    -- space between code and error text
+    },
+    signs = true,        -- keep sign column icons
+    underline = true,    -- underline the error range
+    update_in_insert = false,
+    severity_sort = true,
+  })
+end
+)
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,61 +1,64 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    -- event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
+
   {
-      "toppair/peek.nvim",
-      build = "demo task --quite build::fast",
-      keys = {
-          {
-              "<lead>op",
-              function()
-              local peek = require("peek")
-                if peek.is_open() then
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>op",
+        function()
+          local peek = require "peek"
+          if peek.is_open() then
             peek.close()
-            else
+          else
             peek.open()
-            end
-          end,
-          desc = "Peek (Markdown Preview)"
-          },
-          opts = { theme = "dark", app = "browser"}
-      }
+          end
+        end,
+        desc = "Peek (Markdown Preview)",
+      },
+    },
+    opts = { theme = "dark", app = "browser" },
   },
+
   {
-      "kdheepak/lazygit.nvim",
-      lazy = false,
+    "kdheepak/lazygit.nvim",
+    lazy = false,
   },
+
   {
     "mfussenegger/nvim-dap",
     lazy = false,
   },
+
   {
     "rcarriga/nvim-dap-ui",
     lazy = false,
     dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
+      local dap = require "dap"
+      local dapui = require "dapui"
       dapui.setup()
 
       -- auto open/close UI
       dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
+        dapui.open()
       end
       dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
+        dapui.close()
       end
     end,
   },
-
+}
 
