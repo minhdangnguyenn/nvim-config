f24f3863b2aa7186c0cbf79a73012183d3728173
minhdangnguyenn <dangminh214@gmail.com>
2026-05-22 23:01:45 +0200

    Stop tracking target directory

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,26 @@
-require("nvchad.configs.lspconfig").defaults()
+-- Use NvChad's default LSP settings (keymaps, capabilities, etc.)
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local servers = { "clangd", "cmake" }  -- add cmake here
+local lspconfig = require "lspconfig"
 
-vim.lsp.enable(
-    servers
-)
+-- List all the servers you want here
+local servers = { "clangd", "cmake", "rust_analyzer" }
+
+for _, server in ipairs(servers) do
+  lspconfig[server].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Cấu hình cách hiển thị diagnostics
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",   -- ký hiệu đứng trước lỗi, có thể đổi thành "" hay ">>"
+    spacing = 2,    -- khoảng cách giữa code và lỗi
+  },
+  signs = true,      -- vẫn giữ chữ E, W ở cột trái
+  underline = true,  -- gạch chân chỗ có lỗi
+  update_in_insert = false,
+})
